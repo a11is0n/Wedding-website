db102e37505eefbfc82f2f122854c2b1a3a161f3
Allison Ko <allisonsko@gmail.com>
2014-05-19 23:13:41 -0700

    Implement styling of active page

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,27 @@
 var $window;
 var $nav, navTop, navHeight, $navPlaceholder;
+var $pageLinks, pageLocations, currentActivePageHash;
 
 $(document).ready(setup);
 
 function setup() {
   $window = $(window);
+
   $nav = $('header > nav');
   navTop = $nav.offset().top;
   navHeight = $nav.height();
   $navPlaceholder = $('header > div#nav_placeholder');
-  $window.scroll(toggleNav);
+
+  $pageLinks = $('header > nav > ul > li > a');
+  pageLocations = {};
+  currentActivePageHash = null;
+  $pageLinks.each(function() {
+    var pageHash = getPageHash($(this));
+    pageLocations[pageHash] = $('#' + pageHash).offset().top;
+  });
+
   $('header > nav > ul > li').on('click', 'a', smoothScroll);
+  $window.scroll(scrollChecks);
 }
 
 function smoothScroll(event) {
@@ -24,6 +35,11 @@ function smoothScroll(event) {
   return false;
 }
 
+function scrollChecks() {
+  toggleNav();
+  updateActivePageLink();
+}
+
 function toggleNav() {
   var shouldStick = $window.scrollTop() > navTop;
   if (shouldStick && !$nav.hasClass('stuck')) {
@@ -33,4 +49,29 @@ function toggleNav() {
     $nav.removeClass('stuck');
     $navPlaceholder.height(0);
   }
+}
+
+function updateActivePageLink() {
+  var $activePageLink = null;
+  var scrollTop = $window.scrollTop();
+  // Iterate in page order
+  $pageLinks.each(function() {
+    var $this = $(this);
+    var pageHash = getPageHash($this);
+    if (scrollTop < pageLocations[pageHash]) {
+      return false;
+    }
+    $activePageLink = $this;
+  });
+  if (getPageHash($activePageLink) !== currentActivePageHash) {
+    $('header > nav > ul > li.active').removeClass('active');
+    if ($activePageLink) {
+      $activePageLink.parent('li').addClass('active');
+    }
+    currentActivePageHash = getPageHash($activePageLink);
+  }
+}
+
+function getPageHash($element) {
+  return $element ? $element.attr('href').replace(/^.*?(#|$)/,'') : null;
 }
